404a170447552fd4b26715cb9e89c45ae9c31644
abahuje <afabiorah@gmail.com>
2023-04-17 08:45:30 +0200

    initialise project

---
+++ index.js
@@ -0,0 +1,21 @@
+function onImage(){
+    this.setAttribute("src","./fab-removebg-preview (4).png")
+}
+function outImage(){
+    this.setAttribute("src","./fab-removebg-preview (4).png")
+}
+function outImage(){
+    this.setAttribute("src","./fab-removebg-preview (4).png")
+}
+function lineDisappear(){
+    document.getElementById("line1").style.textDecoration="none";
+    
+}
+function lineAppear(){
+    document.getElementById("line1").style.textDecoration="underline";
+    document.getElementById("line1")=style.textDecorationColor="aqua";
+}
+
+function learnMore(){
+    document.querySelector("graph").style.display="flex"
+}
